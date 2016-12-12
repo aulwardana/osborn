70db907721df3e97a784effb7fba4ea589190235
Aul Wardana <auldesain@gmail.com>
2016-12-12 13:16:32 +0700

    add main.go for testing module, library, or driver

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := "8000"
+
+	http.HandleFunc("/req", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "%s %s\n", r.Host, r.URL.Path)
+	})
+
+	log.Println("Server started: http://localhost:" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
