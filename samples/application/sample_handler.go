32845cc0eeea31ef16fa31dd93d63b3de67e79a5
Aulia Arif Wardana <auldesain@gmail.com>
2017-03-12 14:29:57 +0700

    add machine profile

--- samples/application/sample_handler.go
+++ samples/application/sample_handler.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/mem"
 )
 
 func server(w http.ResponseWriter, r *http.Request) {
@@ -11,4 +14,10 @@ func server(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Postgres : %s:%d\n", cnf.Postgres().Host, cnf.Postgres().Port)
 	fmt.Fprintf(w, "Redis : %s:%d\n", cnf.Redis().Host, cnf.Redis().Port)
 	fmt.Fprintf(w, "Mqtt : %s:%d\n", cnf.Mqtt().Url, cnf.Mqtt().Port)
+
+	v, _ := mem.VirtualMemory()
+	h, _ := host.Info()
+
+	fmt.Fprintf(w, "\nTotal Memory: %v, Free Memory:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
+	fmt.Fprintf(w, "OS: %v, Kernel Version:%v, Uptime:%d\n", h.OS, h.KernelVersion, h.Uptime)
 }
